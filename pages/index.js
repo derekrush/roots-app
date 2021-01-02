d839bf86523e16b90503eb2e7d6706d462f18296
Derek Rush <derekrush.okc@gmail.com>
2021-01-01 20:33:34 -0500

    Added EmptyState to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
-import { Heading, Page } from "@shopify/polaris";
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+    <Layout>
+      <EmptyState
+        heading="Woo Reports!"
+        action={{
+          content: 'OK, let\'s get started.',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>There are likely no reports here, yet.</p>
+      </EmptyState>
+    </Layout>
   </Page>
 );
 
